7a25a55d55c19f867a582f590b013e69d65918f2
qkzan <qkzan@users.noreply.github.com>
2020-07-25 23:03:28 -0300

    Create MIthreshold.m

---
+++ Power_Coupling/MIthreshold.m
@@ -0,0 +1,19 @@
+function [result] =  MIthreshold(MI,meanMI_surr,MIsdt_surr)
+
+% [MI,p,bins,step] = couplingMI2(Pha,Amp,'pa');
+% N_surr = 500;
+% 
+% mi_surr = MI; % rever
+% 
+% MI_surr = mean(mi_surr);
+% MIsdt_surr = std(mi_surr);
+
+z_surr = (MI - meanMI_surr)/MIsdt_surr;
+
+z_threshold = 1.65; % para um teste unicaudal, com intervalo de confiança de 95%
+
+if z_surr > z_threshold
+    result = 1; % sim, foi significativo
+else 
+    result = 0; % não, não foi significativo
+end
