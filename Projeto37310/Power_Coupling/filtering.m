f7e0a92d0a82555fc2f5459819d6e944a1e03057
qkzan <qkzan@users.noreply.github.com>
2020-08-20 10:51:42 -0300

    reorganizando pastas

--- Projeto37310/Power_Coupling/filtering.m
+++
@@ -1,8 +0,0 @@
-function y = filtering(x,F_range,srate)
-    y=table;
-    for f = 1:size(F_range,1)
-        lf = F_range{f}(1);
-        hf = F_range{f}(3);
-        y.(f) = eegfilt(x,srate,lf,hf,0,4*fix(srate/lf));
-    end
-end
